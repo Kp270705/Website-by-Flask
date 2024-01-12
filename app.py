a2c25a834c63075fb467edfcc28b54f01b2bad20
Kp270705 <kunalpathak4774@gmail.com>
2024-01-12 13:49:46 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -1,52 +1,4 @@
 
-# from flask import Flask, render_template, jsonify      # there is a class "flask" inside module 'flask'
-# from flask_sqlalchemy import SQLAlchemy
-# from datetime import datetime 
-
-
-# app = Flask(__name__)
-
-
-# jobs = [
-#     {
-#         'id':1,
-#         'title': 'Data-Analyst',
-#         'Location':'Delhi, India',
-#         'Salary':'200000'
-
-#     },
-
-#     {
-#         'id':2,
-#         'title': 'Data-Scientist',
-#         'Location':'Gurugram, India',
-#         'Salary':'400000'
-
-#     },
-
-#     {
-#         'id':3,
-#         'title': 'Product-Manager',
-#         'Location':'Silcher, India',
-#         'Salary':'80000'
-
-#     },
-# ]
-
-# @app.route('/')
-# def hello_world():
-#     return render_template('index.html', JOBS=jobs)
-
-# @app.route('/jobs')
-# def list_jobs():
-#     return jsonify(jobs)
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=8000)
-
-
-# ============================================================================
 
 # from typing import Self
 from flask import Flask, render_template, jsonify      # there is a class "flask" inside module 'flask'
@@ -72,7 +24,8 @@ def list_jobs():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, port=8000)
-    app.run(host='0.0.0.0', debug=True)
+    
+    app.run(debug=True, port=8000)
+    # app.run(host='0.0.0.0', debug=True)
 
 
